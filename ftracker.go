55829f698117aa7b0e41d98b7910e47441d8d4c0
Demetrz <zaichkin1dm@gmail.com>
2024-11-19 01:06:06 +0300

    1day-2iteration

--- ftracker.go
+++ ftracker.go
@@ -82,7 +82,7 @@ const (
 // duration float64 — длительность тренировки в часах.
 func RunningSpentCalories(action int, weight, duration float64) float64 {
     // ваш код здесь
-    ...
+    runSpent := (runningCaloriesMeanSpeedMultiplier*meanSpeed(action, duration)*runningCaloriesMeanSpeedShift)*weight/mInKm*duration*minInH
 }
 
 // Константы для расчета калорий, расходуемых при ходьбе.
@@ -101,7 +101,7 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
     // ваш код здесь
-    walkSpend := ((walkingCaloriesWeightMultiplier*weight + (math.Pow(meanSpeed(action, duration), 2)/height)*walkingSpeedHeightMultiplier*weight)*duration*minInH)
+    walkSpend := (walkingCaloriesWeightMultiplier*weight + (math.Pow(meanSpeed(action, duration), 2)/height)*walkingSpeedHeightMultiplier*weight)*duration*minInH
     return walkSpend
 }
 
